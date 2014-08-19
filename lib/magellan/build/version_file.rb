0c938f7d42133039b25cd0741e0c9e70a13aa69b
akima <akm2000@gmail.com>
2014-08-19 19:35:54 +0900

    implement these 3 version command
    magellan-build version bump
    magellan-build version major
    magellan-build version minor

--- lib/magellan/build/version_file.rb
+++ lib/magellan/build/version_file.rb
@@ -11,6 +11,44 @@ module Magellan
       def init
         File.open(FILENAME, "w"){|f| f.puts("0.0.1") }
       end
+
+      desc "major [NUMBER]", "bump up major version of VERSION file"
+      def major(num = nil)
+        bump_on(0, num)
+      end
+
+      desc "minor [NUMBER]", "bump up minor version of VERSION file"
+      def minor(num = nil)
+        bump_on(1, num)
+      end
+
+      desc "bump [NUMBER]", "bump up last number of VERSION file"
+      def bump(num = nil)
+        bump_on(2, num)
+      end
+
+      def read_file
+        File.read(FILENAME).strip
+      end
+      private :read_file
+
+      def write_file(version)
+        File.open(FILENAME, "w"){|f| f.puts(version) }
+      end
+      private :write_file
+
+      def bump_on(pos, num)
+        current = read_file
+        body, suffix = current.split(/-/, 2)
+        parts = body.split(/\./)
+        parts[pos] = num || (parts[pos].to_i + 1).to_s
+        ver = parts.join(".")
+        ver << "-" << suffix if suffix
+        write_file(ver)
+        ver
+      end
+      private :bump_on
+
     end
   end
 end
---
+++ spec/magellan/build/version_file_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Magellan::Build::VersionFile do
+
+  let(:subject){ Magellan::Build::VersionFile.new }
+
+  {
+    major: {
+      without_arg: "2.2.3",
+      with_arg: "5.2.3",
+    },
+    minor: {
+      without_arg: "1.3.3",
+      with_arg: "1.5.3",
+    },
+    bump: {
+      without_arg: "1.2.4",
+      with_arg: "1.2.5",
+    },
+  }.each do |method_name, patterns|
+
+    describe method_name do
+      it :without_arg do
+        allow(subject).to receive(:read_file).and_return("1.2.3")
+        expect(subject).to receive(:write_file).with(patterns[:without_arg])
+        subject.send(method_name)
+      end
+
+      it :with_arg do
+        allow(subject).to receive(:read_file).and_return("1.2.3")
+        expect(subject).to receive(:write_file).with(patterns[:with_arg])
+        subject.send(method_name, "5")
+      end
+    end
+
+  end
+
+end
