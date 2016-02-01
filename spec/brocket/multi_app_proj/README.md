e2a3be110a87af928497b50e72ede56bb79aa5e6
akm <akm2000@gmail.com>
2016-02-01 12:09:01 +0900

    Add multi_app_proj for the container which has some applications

---
+++ spec/brocket/multi_app_proj/README.md
@@ -0,0 +1,3 @@
+"app1" is old application and "app2" is newer application.
+
+"app2" works with "app1" on the container. So the container of app2 includes app1.
---
+++ spec/brocket/multi_app_proj_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe :multi_app_proj do
+  context :app1 do
+    let(:dockerfile){ File.expand_path("../multi_app_proj/app1/docker/Dockerfile", __FILE__) }
+    let(:version_filepath){ File.expand_path("../multi_app_proj/app1/docker/VERSION.txt", __FILE__) }
+    
+    describe BRocket::VersionFile do
+      let(:version_obj) do
+        obj = BRocket::VersionFile.new
+        obj.options = {"dockerfile" => dockerfile}
+        obj
+      end
+      it{ expect(version_obj.current).to eq "1.2.1" }
+    end
+  end
+
+  context :app1 do
+    let(:dockerfile){ File.expand_path("../multi_app_proj/app2/Dockerfile", __FILE__) }
+    let(:version_filepath){ File.expand_path("../multi_app_proj/app2/VERSION", __FILE__) }
+    
+    describe BRocket::VersionFile do
+      let(:version_obj) do
+        obj = BRocket::VersionFile.new
+        obj.options = {"dockerfile" => dockerfile}
+        obj
+      end
+      it{ expect(version_obj.current).to eq "0.1.0" }
+    end
+  end
+end
