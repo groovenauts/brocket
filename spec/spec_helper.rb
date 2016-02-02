e3ab530775fff7cde49aa257df1bd384e0b5491d
akm <akm2000@gmail.com>
2016-02-02 14:09:09 +0900

    Enable simplecov with COVERAGE is true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+if ENV["COVERAGE"] =~ /true|yes|on|1/i
+  require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::RcovFormatter
+  ]
+  SimpleCov.start do
+    add_filter "spec/"
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'brocket'
 
@@ -5,3 +17,4 @@ logpath = File.expand_path("../../tmp/test.log", __FILE__)
 require 'fileutils'
 FileUtils.mkdir_p(File.dirname(logpath))
 BRocket.logger = Logger.new(logpath)
+
