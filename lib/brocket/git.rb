734b83c8e40fed47d3afbd8c1619d71e6a0bd8a8
akm <akm2000@gmail.com>
2016-02-02 16:18:51 +0900

    Remove unused method #commit

--- lib/brocket/git.rb
+++ lib/brocket/git.rb
@@ -20,11 +20,6 @@ module BRocket
     end
 
     no_commands do
-
-      def commit(filepath, msg)
-        sh("git add #{filepath} && git commit -m \"#{msg}\"")
-      end
-
       def clean?
         sh("git diff --exit-code")
         return true
