f154ea9c55b8596f79838a6b5a0ca2f39a7a60a0
akm <akm2000@gmail.com>
2018-02-12 11:31:39 +0900

    :shower: Remove ruby implementation

--- lib/brocket/version.rb
+++
@@ -1,3 +0,0 @@
-module BRocket
-  VERSION = "0.2.4"
-end
--- spec/brocket_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe BRocket do
-  it 'has a version number' do
-    expect(BRocket::VERSION).not_to be nil
-  end
-end
