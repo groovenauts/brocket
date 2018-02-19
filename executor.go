dd4108e24a40895768088f4c1ad096abdad12098
akm <akm2000@gmail.com>
2018-02-19 20:32:57 +0900

    :+1: Add ExecRun and ExecOutout to call methods in exec via Executor

--- executor.go
+++ executor.go
@@ -6,7 +6,7 @@ import (
 
 type Executor interface {
 	Run(cmd *exec.Cmd) error
-	Output(cmd *exec.Cmd) error
+	Output(cmd *exec.Cmd) ([]byte, error)
 }
 
 type DefaultExecutor struct {
@@ -18,4 +18,19 @@ func (ex *DefaultExecutor) Run(cmd *exec.Cmd) error {
 
 func (ex *DefaultExecutor) Output(cmd *exec.Cmd) ([]byte, error) {
 	return cmd.Output()
+}
+
+func (c *Configuration) GetExecutor() Executor {
+	if c.executor == nil {
+		c.executor = &DefaultExecutor{}
+	}
+	return c.executor
+}
+
+func (c *Configuration) ExecRun(cmd *exec.Cmd) error {
+	return c.GetExecutor().Run(cmd)
+}
+
+func (c *Configuration) ExecOutput(cmd *exec.Cmd) ([]byte, error) {
+	return c.GetExecutor().Output(cmd)
 }
